128f1414a6ce6c03cc3d75d99c6b10df4b486c36
Aidan-Z <aidanzamfir@gmail.com>
2023-10-19 10:34:45 -0300

    main msg

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import secrets
 
-MSG = "Group Works TEST2 !"
+MSG = "NEW FILE (FlightAPP) TEST"
 
 def main():
     xx = f"https://api.telegram.org/bot{secrets.TELE_TOK}/sendMessage?chat_id=@{secrets.GROUP_ID}&text={MSG}"
