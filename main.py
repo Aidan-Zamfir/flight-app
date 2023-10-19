cc806bd0c1139921680748dd62b2f83b83d16c02
Aidan-Z <aidanzamfir@gmail.com>
2023-10-19 10:34:08 -0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+import secrets
 
+MSG = "Group Works TEST2 !"
+
+def main():
+    xx = f"https://api.telegram.org/bot{secrets.TELE_TOK}/sendMessage?chat_id=@{secrets.GROUP_ID}&text={MSG}"
+    x = requests.get(xx)
+
+
+
+if __name__ == "__main__":
+    main()
